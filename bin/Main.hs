322d844e98728ae0a64eaf43cc1145b8649dd213
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2020-10-22 02:47:14 +0200

    Parse basic Metro files

--- bin/Main.hs
+++ bin/Main.hs
@@ -1,14 +1,12 @@
 module Main where
 
 import System.Environment
-import MetroLang.WebAssembly.Parser
-import MetroLang.WebAssembly.Generator
+import MetroLang.Parser
 
 -- | main, do nothing
 main :: IO ()
 main =
     do  args <- getArgs
-        let arg1:arg2:_ = args
+        let arg1:_ = args
         ast <- parseFile arg1
         print ast
-        generateFile arg2 ast
---
+++ src/MetroLang/Parser.hs
@@ -0,0 +1,182 @@
+module MetroLang.Parser (parseString, parseFile) where
+
+import Control.Monad (liftM)
+import Data.Functor.Identity
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token as Token
+import MetroLang.AST
+
+languageDef :: GenLanguageDef String u Identity
+languageDef =
+  emptyDef { Token.commentStart    = "/*"
+           , Token.commentEnd      = "*/"
+           , Token.commentLine     = "//"
+           , Token.identStart      = letter <|> oneOf "_"
+           , Token.identLetter     = alphaNum <|> oneOf "_"
+           , Token.reservedNames   = [ "if"
+                                     , "else"
+                                     , "fn"
+                                     , "true"
+                                     , "false"
+                                     , "null"
+                                     ]
+           , Token.reservedOpNames = [ ","
+                                     , "."
+                                     , "("
+                                     , ")"
+                                     , "{"
+                                     , "}"
+                                     , "or"
+                                     , "and"
+                                     , "+"
+                                     , "-"
+                                     , "*"
+                                     , "/"
+                                     , "%"
+                                     , "="
+                                     , "=="
+                                     , "!="
+                                     , ">="
+                                     , ">"
+                                     , "<"
+                                     ]
+           }
+
+lexer       :: Token.GenTokenParser String u Identity
+lexer       = Token.makeTokenParser languageDef
+
+identifier  :: Parser Identifier
+identifier  = Token.identifier lexer
+
+-- | reserved parses a reserved word
+reserved    :: String -> Parser ()
+reserved    = Token.reserved lexer
+
+-- | reservedOp parses an operator
+reservedOp  :: String -> Parser ()
+reservedOp  = Token.reservedOp lexer
+
+-- | parens parses parentheses around an expression
+parens      :: Parser a -> Parser a
+parens      = Token.parens     lexer
+
+-- | integer parses an integer
+integer     :: Parser Integer
+integer     = Token.integer lexer
+
+-- | whiteSpace parses whitespace
+whiteSpace  :: Parser ()
+whiteSpace  = Token.whiteSpace lexer
+
+comma :: Parser ()
+comma = reservedOp ","
+
+whileParser :: Parser Module
+whileParser = whiteSpace >> moduleParser
+
+moduleParser :: Parser Module
+moduleParser =
+  do  values <- many declaration
+      return $ Mod values
+
+declaration :: Parser Declaration
+declaration = funcDeclaration
+
+funcDeclaration :: Parser Declaration
+funcDeclaration =
+  do  reserved "fn"
+      iden <- identifier
+      pars <- params
+      body <- block
+      return $ Func iden pars body
+
+params :: Parser Params
+params =
+  do  reservedOp "("
+      pars <- sepBy param comma
+      optional comma
+      reservedOp ")"
+      return pars
+
+param :: Parser Param
+param =
+  do  iden <- identifier
+      typ <- identifier
+      return $ Par iden typ
+
+block :: Parser Block
+block =
+  do  reservedOp "{"
+      s <- many stmt
+      reservedOp "}"
+      return $ Block s
+
+stmt :: Parser Stmt
+stmt =   liftM IfStmt ifParser
+     <|> liftM ExprStmt expr
+
+ifParser :: Parser If
+ifParser =
+  do  reserved "if"
+      cond <- expr
+      thenBlock <- block
+      e <- optionMaybe elseParser
+      return $ If cond thenBlock e
+
+elseParser :: Parser Else
+elseParser =
+  do  reserved "else"
+      (liftM ElseIfStmt ifParser) <|> (liftM ElseStmt block)
+
+expr :: Parser Expression
+expr = buildExpressionParser operators term
+
+operators :: [[Operator Char () Expression]]
+operators = [ [Prefix (reservedOp "-"   >> return (Neg              ))          ]
+            , [Infix  (reservedOp "*"   >> return (Binary Multiply  )) AssocLeft,
+               Infix  (reservedOp "/"   >> return (Binary Divide    )) AssocLeft,
+               Infix  (reservedOp "%"   >> return (Binary Modulo    )) AssocLeft]
+            , [Infix  (reservedOp "+"   >> return (Binary Add       )) AssocLeft,
+               Infix  (reservedOp "-"   >> return (Binary Subtract  )) AssocLeft]
+            , [Infix  (reservedOp "and" >> return (Binary LogicalAnd)) AssocLeft,
+               Infix  (reservedOp "or"  >> return (Binary LogicalOr )) AssocLeft]
+            ]
+
+term :: Parser Expression
+term =   parens expr
+     <|> nullLiteral
+     <|> liftM BooleanLiteral booleanLiteral
+     <|> liftM VariableExpr identifier
+     <|> liftM NumberLiteral integer
+     <|> liftM StringLiteral stringLiteral
+
+booleanLiteral :: Parser Bool
+booleanLiteral =   (reserved "true" >> (return True))
+               <|> (reserved "false" >> (return False))
+
+
+stringLiteral :: Parser String
+stringLiteral =
+  do  _ <- oneOf "\""
+      stringValue <- many (noneOf "\"")
+      _ <- oneOf "\""
+      whiteSpace
+      return stringValue
+
+nullLiteral :: Parser Expression
+nullLiteral = reserved "null" >> (return NullLiteral)
+
+parseString :: String -> Module
+parseString str =
+  case parse whileParser "" str of
+    Left e  -> error $ show e
+    Right r -> r
+
+parseFile :: String -> IO Module
+parseFile file =
+  do program  <- readFile file
+     case parse whileParser "" program of
+       Left e  -> print e >> fail "parse error"
+       Right r -> return r
