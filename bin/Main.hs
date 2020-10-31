77e0c1ae20fde6881417cfc8818f5a65fc019efb
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2020-10-31 14:53:18 +0100

    Compile WASM text to binary format

--- bin/Main.hs
+++ bin/Main.hs
@@ -2,7 +2,10 @@
 module Main where
 
 import Data.FileEmbed
+import System.Directory
 import System.Environment
+import System.FilePath.Posix
+import System.Process
 
 import MetroLang.Compilation.Compile
 import MetroLang.Parser as Metro
@@ -10,16 +13,29 @@ import MetroLang.WebAssembly.Generator
 import MetroLang.WebAssembly.Parser as WASM
 import MetroLang.WebAssembly.Utils
 
+-- | watToWasm compiles WebAssembly Text format to Binary format
+watToWasm :: String -> String -> IO ()
+watToWasm inFile outFile = callProcess "wat2wasm" [inFile, "-o", outFile]
+
 -- | main, do nothing
 main :: IO ()
 main =
   do  args <- getArgs
-      let arg1:arg2:_ = args
+      let inputFile:outDir:_ = args
+          baseName = takeBaseName inputFile
+          outWatFile = outDir </> baseName ++ ".wat"
+          outWasmFile = outDir </> baseName ++ ".wasm"
+
+      -- Create output directory if it is missing
+      createDirectoryIfMissing True outDir
 
       -- Load std lib
       std <- return $ WASM.parseString $(embedStringFile "std/std.wat")
 
-      -- Compile program
-      ast <- Metro.parseFile arg1
+      -- Compile program and output WebAssembly Text format
+      ast <- Metro.parseFile inputFile
       wasm <- return $ compile ast
-      generateFile arg2 $ merge std wasm
+      generateFile outWatFile $ merge std wasm
+
+      -- Compile WebAssembly Text format to WebAssembly Binary format
+      watToWasm outWatFile outWasmFile
