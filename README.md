91bba36b6fa58b56d00658b0d77c3cab0442522f
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2020-10-31 16:03:57 +0100

    Print help text and version

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ Metro Language
 ![Haskell CI](https://github.com/ksm2/metroc/workflows/Haskell%20CI/badge.svg)
 
 **Metro** is a garbage collected, compiled language for the Web and Server.
+
+Usage
+-----
+
+```
+Metro Compiler 0.1.0
+
+SYNOPSIS
+  metroc COMMAND
+  metroc [-h|--help]
+  metroc [-v|--version]
+
+META COMMANDS
+  metroc help       Print this help text and exit.
+  metroc version    Display the version number and exit.
+```
--- bin/Main.hs
+++ bin/Main.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Main where
+module Main(main) where
 
 import Data.FileEmbed
+import Data.Version
+import Paths_metroc (version)
 import System.Directory
 import System.Environment
 import System.FilePath.Posix
+import System.Posix.Terminal (queryTerminal)
+import System.Posix.IO (stdOutput)
 import System.Process
 
 import MetroLang.Compilation.Compile
@@ -13,15 +17,47 @@ import MetroLang.WebAssembly.Generator
 import MetroLang.WebAssembly.Parser as WASM
 import MetroLang.WebAssembly.Utils
 
--- | watToWasm compiles WebAssembly Text format to Binary format
-watToWasm :: String -> String -> IO ()
-watToWasm inFile outFile = callProcess "wat2wasm" [inFile, "-o", outFile]
+joinDot :: (Show a) => [a] -> String
+joinDot [] = ""
+joinDot [element] = show element
+joinDot (x:xs) = (show x) ++ "." ++ (joinDot xs)
 
--- | main, do nothing
-main :: IO ()
-main =
-  do  args <- getArgs
-      let inputFile:outDir:_ = args
+primaryLn :: String -> IO ()
+primaryLn text =
+  do  isTty <- queryTerminal stdOutput
+      putStrLn $ if isTty then "\x1b[1;38;2;216;83;105m" ++ text ++ "\x1b[0m" else text
+
+boldLn :: String -> IO ()
+boldLn text =
+  do  isTty <- queryTerminal stdOutput
+      putStrLn $ if isTty then "\x1b[1m" ++ text ++ "\x1b[0m" else text
+
+printInfo :: IO ()
+printInfo =
+  do  progName <- getProgName
+      primaryLn $ "Metro Compiler " ++ joinDot (versionBranch version)
+      putStrLn ""
+
+      boldLn "SYNOPSIS"
+      putStrLn $ "  " ++ progName ++ " COMMAND"
+      putStrLn $ "  " ++ progName ++ " [-h|--help]"
+      putStrLn $ "  " ++ progName ++ " [-v|--version]"
+      putStrLn ""
+
+      boldLn "META COMMANDS"
+      putStrLn $ "  " ++ progName ++ " help       Print this help text and exit."
+      putStrLn $ "  " ++ progName ++ " version    Display the version number and exit."
+
+printHelp :: IO ()
+printHelp =
+  do  printInfo
+
+printVersion :: IO ()
+printVersion = putStr "v" >> (putStrLn . joinDot . versionBranch) version
+
+build :: [String] -> IO ()
+build args =
+  do  let inputFile:outDir:_ = args
           baseName = takeBaseName inputFile
           outWatFile = outDir </> baseName ++ ".wat"
           outWasmFile = outDir </> baseName ++ ".wasm"
@@ -39,3 +75,21 @@ main =
 
       -- Compile WebAssembly Text format to WebAssembly Binary format
       watToWasm outWatFile outWasmFile
+
+-- | watToWasm compiles WebAssembly Text format to Binary format
+watToWasm :: String -> String -> IO ()
+watToWasm inFile outFile = callProcess "wat2wasm" [inFile, "-o", outFile]
+
+-- | main, do nothing
+main :: IO ()
+main =
+  do  args <- getArgs
+      case args of
+        ("version":_)   -> printVersion
+        ("--version":_) -> printVersion
+        ("-v":_)        -> printVersion
+        ("help":_)      -> printHelp
+        ("--help":_)    -> printHelp
+        ("-h":_)        -> printHelp
+        []              -> printHelp
+        _ -> build args
