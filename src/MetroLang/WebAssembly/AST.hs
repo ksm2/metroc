1bd13d9f80369ad1fd0c9b19f999d1042bb9d892
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2020-11-01 14:17:32 +0100

    Ensure correct order of WebAssembly declarations

--- src/MetroLang/WebAssembly/AST.hs
+++ src/MetroLang/WebAssembly/AST.hs
@@ -13,6 +13,52 @@ data Declaration = Import StringLiteral StringLiteral ImportSpecifier
                  | Start Identifier
                    deriving (Show)
 
+instance Eq Declaration where
+  (Import a1 b1 _) == (Import a2 b2 _) = a1 == a2 && b1 == b2
+  (Memory a1 _) == (Memory a2 _) = a1 == a2
+  (Export a1 _) == (Export a2 _) = a1 == a2
+  (Global a1 _ _) == (Global a2 _ _) = a1 == a2
+  (Func a1 _ _ _) == (Func a2 _ _ _) = a1 == a2
+  (Start a1) == (Start a2) = a1 == a2
+  _ == _ = False
+
+instance Ord Declaration where
+  (Import _ _ _) <= (Import _ _ _) = True
+  (Import _ _ _) <= (Memory _ _) = True
+  (Import _ _ _) <= (Export _ _) = True
+  (Import _ _ _) <= (Global _ _ _) = True
+  (Import _ _ _) <= (Data _ _) = True
+  (Import _ _ _) <= (Func _ _ _ _) = True
+  (Import _ _ _) <= (Start _) = True
+
+  (Memory _ _) <= (Memory _ _) = True
+  (Memory _ _) <= (Export _ _) = True
+  (Memory _ _) <= (Global _ _ _) = True
+  (Memory _ _) <= (Data _ _) = True
+  (Memory _ _) <= (Func _ _ _ _) = True
+  (Memory _ _) <= (Start _) = True
+
+  (Export _ _) <= (Export _ _) = True
+  (Export _ _) <= (Global _ _ _) = True
+  (Export _ _) <= (Data _ _) = True
+  (Export _ _) <= (Func _ _ _ _) = True
+  (Export _ _) <= (Start _) = True
+
+  (Global _ _ _) <= (Global _ _ _) = True
+  (Global _ _ _) <= (Data _ _) = True
+  (Global _ _ _) <= (Func _ _ _ _) = True
+  (Global _ _ _) <= (Start _) = True
+
+  (Data _ _) <= (Data _ _) = True
+  (Data _ _) <= (Func _ _ _ _) = True
+  (Data _ _) <= (Start _) = True
+
+  (Func _ _ _ _) <= (Func _ _ _ _) = True
+  (Func _ _ _ _) <= (Start _) = True
+
+  (Start _) <= (Start _) = True
+  _ <= _ = False
+
 data ImportSpecifier = IFunc Identifier [Param] ReturnType
                        deriving (Show)
 
--- src/MetroLang/WebAssembly/Generator.hs
+++ src/MetroLang/WebAssembly/Generator.hs
@@ -1,6 +1,7 @@
 module MetroLang.WebAssembly.Generator (generateString, generateFile) where
 
 import Data.Char (isSpace)
+import Data.List
 import MetroLang.Bytes (toString)
 import MetroLang.WebAssembly.AST
 
@@ -8,16 +9,16 @@ generateModule :: Module -> String
 generateModule (Mod d) = wrap "module" [declarations d]
 
 declarations :: [Declaration] -> String
-declarations = indent2 . map declaration
+declarations = indent2 . (map declaration) . sort
 
 declaration :: Declaration -> String
 declaration (Import m i s) = wrap "import" [stringLiteral m, stringLiteral i, importSpecifier s]
-declaration (Memory iden i) = wrap "memory" [identifier iden, show i]
+declaration (Memory iden i) = newline $ wrap "memory" [identifier iden, show i]
 declaration (Export e s) = wrap "export" [stringLiteral e, exportSpecifier s]
 declaration (Global i gt e) = wrap "global" [identifier i, globaltype gt, expr e]
 declaration (Data e s) = wrap "data" [expr e, toString s]
-declaration (Func iden p r s) = wrap "func" [identifier iden, params p, returnType r, stmt s]
-declaration (Start iden) = wrap "start" [identifier iden]
+declaration (Func iden p r s) = newline $ wrap "func" [identifier iden, params p, returnType r, stmt s]
+declaration (Start iden) = newline $ wrap "start" [identifier iden]
 
 importSpecifier :: ImportSpecifier -> String
 importSpecifier (IFunc iden p r) = wrap "func" [identifier iden, params p, returnType r]
@@ -75,6 +76,9 @@ prefix pref str = pref ++ str
 
 indent :: String -> String -> String
 indent identWith str = unlines $ map (prefix identWith) $ lines str
+
+newline :: String -> String
+newline str = '\n' : str
 
 wrap :: String -> [String] -> String
 wrap str content = parens $ str:content
